2aa5efd1d4b8a2daf5a29df8725df0f60c04665b
nakere424 <nakere4@gmail.com>
2023-01-12 14:28:27 +0700

    Add check max_x, max_y before crop

--- utils/crop.py
+++ utils/crop.py
@@ -5,8 +5,13 @@ def random_crop(image, crop_height, crop_width):
     max_x = image.shape[1] - crop_width
     max_y = image.shape[0] - crop_height
 
-    x = np.random.randint(0, max_x)
-    y = np.random.randint(0, max_y)
+    x = y = 0
+
+    if max_x > 0:
+        x = np.random.randint(0, max_x)
+
+    if max_y > 0:
+        y = np.random.randint(0, max_y)
 
     crop = image[y : y + crop_height, x : x + crop_width]
 
