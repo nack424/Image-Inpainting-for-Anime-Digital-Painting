4335c17cc597e9d088535f402fe434024f3a8f9b
nakere424 <nakere4@gmail.com>
2023-01-18 11:24:09 +0700

    Change gradient loss to automatically detect whether input is in cuda or not

--- utils/loss.py
+++ utils/loss.py
@@ -38,25 +38,28 @@ class Coarse_loss:
 
 
 class Gradient_loss:
-    def __init__(self, use_gpu = False):
-        if use_gpu:
-            device = 'cuda'
-        else:
-            device = 'cpu'
-
+    def __init__(self):
         self.horizontal_filter = torch.tensor([
-            [-1, 1]], dtype=torch.float32, device = device).unsqueeze(0).repeat(3, 1, 1).unsqueeze(
+            [-1, 1]], dtype=torch.float32).unsqueeze(0).repeat(3, 1, 1).unsqueeze(
             0)
 
         self.vertical_filter = torch.tensor([
             [-1],
-            [1]], dtype=torch.float32, device = device).unsqueeze(0).repeat(3, 1, 1).unsqueeze(
+            [1]], dtype=torch.float32).unsqueeze(0).repeat(3, 1, 1).unsqueeze(
             0)
 
         self.horizontal_padder = nn.ZeroPad2d((0, 1, 0, 0)) #For same padding
         self.vertical_padder = nn.ZeroPad2d((0, 0, 0, 1))
 
     def __call__(self, predict, groundtruth):
+        if predict.is_cuda:
+            device = 'cuda'
+        else:
+            device = 'cpu'
+
+        self.horizontal_filter, self.vertical_filter = self.horizontal_filter.to(device), self.vertical_filter.to(device)
+        self.horizontal_padder, self.vertical_padder = self.horizontal_padder.to(device), self.vertical_padder.to(device)
+
         different = predict - groundtruth
 
         horizontal_grad = F.conv2d(self.horizontal_padder(different), self.horizontal_filter)
@@ -78,11 +81,11 @@ class Generator_loss:
         return generator_loss
 
 class Joint_refinement_loss:
-    def __init__(self, vgg_model, vgg_loss_weight = 1e-5, gan_loss_weight = 0.5, gradient_loss_weight = 1, use_gpu = True):
+    def __init__(self, vgg_model, vgg_loss_weight = 1e-5, gan_loss_weight = 0.5, gradient_loss_weight = 1):
         self.coarse_loss = Coarse_loss(vgg_model, vgg_loss_weight)
         self.gan_loss = Generator_loss()
         self.gan_loss_weight = gan_loss_weight
-        self.gradient_loss = Gradient_loss(use_gpu)
+        self.gradient_loss = Gradient_loss()
         self.gradient_loss_weight = gradient_loss_weight
 
     def __call__(self, predict, groundtruth, fake_prediction):
