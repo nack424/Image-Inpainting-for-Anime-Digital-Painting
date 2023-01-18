485bdf4702d673cb78c8aefd62f4a24c6cf97c16
nakere424 <nakere4@gmail.com>
2023-01-19 03:11:28 +0700

    Change LeakyReLu slope to 0.2

--- network.py
+++ network.py
@@ -293,11 +293,11 @@ class RefinementNet(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.spectral_conv1 = nn.Sequential(spectral_norm(nn.Conv2d(4, 64, 5, 2, 2, 1)), nn.LeakyReLU())
-        self.spectral_conv2 = nn.Sequential(spectral_norm(nn.Conv2d(64, 128, 5, 2, 2, 1)), nn.LeakyReLU())
-        self.spectral_conv3 = nn.Sequential(spectral_norm(nn.Conv2d(128, 256, 5, 2, 2, 1)), nn.LeakyReLU())
-        self.spectral_conv4 = nn.Sequential(spectral_norm(nn.Conv2d(256, 256, 5, 2, 2, 1)), nn.LeakyReLU())
-        self.spectral_conv5 = nn.Sequential(spectral_norm(nn.Conv2d(256, 256, 5, 2, 2, 1)), nn.LeakyReLU())
+        self.spectral_conv1 = nn.Sequential(spectral_norm(nn.Conv2d(4, 64, 5, 2, 2, 1)), nn.LeakyReLU(0.2))
+        self.spectral_conv2 = nn.Sequential(spectral_norm(nn.Conv2d(64, 128, 5, 2, 2, 1)), nn.LeakyReLU(0.2))
+        self.spectral_conv3 = nn.Sequential(spectral_norm(nn.Conv2d(128, 256, 5, 2, 2, 1)), nn.LeakyReLU(0.2))
+        self.spectral_conv4 = nn.Sequential(spectral_norm(nn.Conv2d(256, 256, 5, 2, 2, 1)), nn.LeakyReLU(0.2))
+        self.spectral_conv5 = nn.Sequential(spectral_norm(nn.Conv2d(256, 256, 5, 2, 2, 1)), nn.LeakyReLU(0.2))
         self.classifier = nn.Linear(65536, 1)
 
     def forward(self, image, mask):
@@ -310,6 +310,6 @@ class Discriminator(nn.Module):
         x = self.spectral_conv5(x)  # Shape batch x 256 x 16 x 16
         x = x.view(-1, 65536)
 
-        output = self.classifier(x)
+        # output = self.classifier(x)
 
-        return output
+        return x
