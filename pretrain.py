63a0a88c920046aa24994600479f02cbbc8740a4
nakere424 <nakere4@gmail.com>
2023-02-03 19:25:23 +0700

    Remove validation step from pretrain

--- pretrain.py
+++ pretrain.py
@@ -20,10 +20,9 @@ parser.add_argument('--load_model', type=str, help='(Optinal) Folder contain sav
 parser.add_argument('--model', type=str, help='Model to train: coarse, super_resolution or refinement')
 parser.add_argument('--save_model', type=str,  help='(Optinal) Folder to save model')
 parser.add_argument('--train_path', type=str,  help='Training image folder')
-parser.add_argument('--val_path', type=str,  help='(Optinal) Validation image folder')
 parser.add_argument('--world_size', type=int, default=1, help='Number of training process (Should be equal to number of GPUs)')
 
-def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, train_path, val_path, save_model):
+def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, train_path, save_model):
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
     vgg19_model = vgg19(weights='IMAGENET1K_V1')
@@ -41,10 +40,6 @@ def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, t
         sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=False, drop_last=True)
         train_dataloader = DataLoader(train_dataset, batch_size = batch_size, sampler=sampler)
 
-        if val_path is not None and is_main_process(): #Only main process to check validation set
-            val_dataset = MaskedDataset(val_path, 1)
-            val_dataloader = DataLoader(val_dataset, batch_size = batch_size)
-
     else:
         model = SuperResolutionNet().to(rank)
 
@@ -53,10 +48,6 @@ def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, t
         train_dataset = SuperResolutionDataset(train_path)
         sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=False, drop_last=True)
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler)
-
-        if val_path is not None and is_main_process():
-            val_dataset = SuperResolutionDataset(val_path)
-            val_dataloader = DataLoader(val_dataset, batch_size = batch_size)
 
     if load_model is not None:
         map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
@@ -78,7 +69,6 @@ def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, t
         train_dataloader.sampler.set_epoch(epoch)
 
         total_train_loss = 0
-        total_val_loss = 0
         num_batch = len(train_dataloader)
 
         for batch, data in enumerate(train_dataloader):
@@ -113,37 +103,10 @@ def pretrain(rank, world_size, batch_size, epochs, lr, load_model, model_name, t
                 scaler.update()
                 ddp_model.zero_grad(set_to_none=True)
 
-        if val_path is not None and is_main_process():
-            for batch, data in enumerate(val_dataloader):
-                if (model == 'coarse') or (model == 'refinement'):
-                    masked_image, mask, groundtruth = data
-                    masked_image, mask, groundtruth = masked_image.to(rank), mask.to(rank), groundtruth.to(rank)
-
-                    with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=True):
-                        output = ddp_model(masked_image, mask)
-                        output = mask * output + (1 - mask) * masked_image
-
-                        loss = loss_function(output, groundtruth)
-                        total_val_loss += loss.item()
-
-                else:
-                    resize_image, groundtruth = data
-                    resize_image, groundtruth = resize_image.to(rank), groundtruth.to(rank)
-
-                    with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=True):
-                        output = ddp_model(resize_image)
-
-                        loss = loss_function(output, groundtruth)
-                        total_val_loss += loss.item()
-
         #log
         average_train_loss = total_train_loss / num_batch
         if is_main_process(): #Only main process to record log
-            if val_path is not None:
-                average_val_loss = total_val_loss / num_batch
-                wandb.log({"train_loss": average_train_loss, "val_loss": average_val_loss})
-            else:
-                wandb.log({"train_loss": average_train_loss})
+            wandb.log({"train_loss": average_train_loss})
 
             #Main process save per 10%
             if save_model is not None and ((100 * (epoch + 1)) / epochs) % 10 == 0:
@@ -158,6 +121,5 @@ if __name__ == '__main__':
     assert cmd_args.train_path is not None
 
     mp.spawn(pretrain, args = (cmd_args.world_size, cmd_args.batch_size, cmd_args.epochs, cmd_args.learning_rate,
-                               cmd_args.load_model, cmd_args.model, cmd_args.train_path, cmd_args.val_path,
-                               cmd_args.save_model),
+                               cmd_args.load_model, cmd_args.model, cmd_args.train_path, cmd_args.save_model),
              nprocs = cmd_args.world_size, join=True)
