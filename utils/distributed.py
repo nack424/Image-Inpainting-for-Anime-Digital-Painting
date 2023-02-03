fdbe97733a6511e68666fb01997a660f332f2656
nakere424 <nakere4@gmail.com>
2023-02-03 20:15:48 +0700

    Add gather function

--- utils/distributed.py
+++ utils/distributed.py
@@ -5,3 +5,12 @@ def get_rank():
 
 def is_main_process():
     return get_rank() == 0
+
+def gather(tensor, tensor_list=None, group=None):
+    if group is None:
+        group = dist.group.WORLD
+    if is_main_process():
+        assert (tensor_list is not None)
+        dist.gather(tensor, gather_list=tensor_list, group=group)
+    else:
+        dist.gather(tensor, dst=0, group=group)
